b91279702a8b0c99c4821f58e108366ee66633ce
minheekangg <minheekangg@gmail.com>
2018-10-31 12:12:51 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,56 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
   get '/' do
 
   end
+
+#NEW
+  get '/posts/new' do
+    erb :new
+  end
+#CREATE
+  post '/posts' do
+    # binding.pry
+    @post = Post.create(params)
+    erb :show
+  end
+
+#INDEX
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+#SHOW
+    get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+#EDIT
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+    # binding.pry
+    erb :edit
+  end
+
+#UPDATE
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.update(params[:post])
+    # binding.pry
+    redirect :"/posts/#{@post.id}"
+  end
+
+  #DELETE
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    # binding.pry
+    Post.delete(@post)
+    erb :delete
+    # binding.pry
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  # attr_accessor :name, :content
+
 end
